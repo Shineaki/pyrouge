0939eb7707439811288510e353c2b0914d24c095
Shineaki <halmosadam92@gmail.com>
2024-12-07 19:16:04 +0100

    Part 12 - increasing difficulty - started

--- pyrouge/procgen.py
+++ pyrouge/procgen.py
@@ -5,9 +5,33 @@ import tcod
 
 import pyrouge.entity_factory
 from pyrouge.engine import Engine
-from pyrouge.entity import Entity
 from pyrouge.game_map import GameMap
 from pyrouge.tile_types import down_stairs, floor
+
+max_items_by_floor = [
+    (1, 1),
+    (4, 2),
+]
+
+max_monsters_by_floor = [
+    (1, 2),
+    (4, 3),
+    (6, 5),
+]
+
+
+def get_max_value_for_floor(
+    max_value_by_floor: list[Tuple[int, int]], floor: int
+) -> int:
+    current_value = 0
+
+    for floor_minimum, value in max_value_by_floor:
+        if floor_minimum > floor:
+            break
+        else:
+            current_value = value
+
+    return current_value
 
 
 class RectangularRoom:
@@ -59,11 +83,13 @@ def tunnel_between(
         yield x, y
 
 
-def place_entities(
-    room: RectangularRoom, dungeon: GameMap, maximum_monsters: int, maximum_items: int
-) -> None:
-    number_of_monsters = random.randint(0, maximum_monsters)
-    number_of_items = random.randint(0, maximum_items)
+def place_entities(room: RectangularRoom, dungeon: GameMap, floor_number: int,) -> None:
+    number_of_monsters = random.randint(
+        0, get_max_value_for_floor(max_monsters_by_floor, floor_number)
+    )
+    number_of_items = random.randint(
+        0, get_max_value_for_floor(max_items_by_floor, floor_number)
+    )
 
     for _ in range(number_of_monsters):
         x = random.randint(room.x1 + 1, room.x2 - 1)
@@ -98,8 +124,6 @@ def generate_dungeon(
     room_max_size: int,
     map_width: int,
     map_height: int,
-    max_monsters_per_room: int,
-    max_items_per_room: int,
     engine: Engine,
 ) -> GameMap:
     """Generate a new dungeon map."""
@@ -138,9 +162,7 @@ def generate_dungeon(
 
             center_of_last_room = new_room.center
 
-        place_entities(new_room, dungeon,
-                       max_monsters_per_room,
-                       max_items_per_room)
+        place_entities(new_room, dungeon, engine.game_world.current_floor)
 
         dungeon.tiles[center_of_last_room] = down_stairs
         dungeon.downstairs_location = center_of_last_room
